fbe400171dac31f4c81a4d55a5e578e2e39ef92c
Prottoy-062 <psovi453@gmail.com>
2025-08-19 23:28:19 +0600

    First commit

---
+++ code1.py
@@ -0,0 +1,8 @@
+#problem 01:Sum of first n natural numbers.
+n = int(input("Enter a number: "))
+total = 0
+for i in range(1, n + 1):
+    total += i
+print("Sum of first", n, "natural numbers is:", total)
+# Numerical-1st-Assesment
+# Numerical-1st-Assesment
